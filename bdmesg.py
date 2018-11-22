ad2357fdc13f042fc050c3a9c435433d8ad3ad9d
CorpNewt <corporatenewt@gmail.com>
2018-11-21 18:38:49 -0600

    Get clover uuid (only works right on GPT drives)

--- bdmesg.py
+++ bdmesg.py
@@ -1,15 +1,33 @@
 import binascii, subprocess
 
+def get_clover_uuid():
+    bd = bdmesg()
+    if not len(bd):
+        return ""
+    # Get bdmesg output - then parse for SelfDevicePath
+    if not "SelfDevicePath=" in bd:
+        # Not found
+        return ""
+    try:
+        # Split to just the contents of that line
+        line = bd.split("SelfDevicePath=")[1].split("\n")[0]
+        # Get the HD section
+        hd   = line.split("HD(")[1].split(")")[0]
+        # Get the UUID
+        uuid = hd.split(",")[2]
+        return uuid
+    except:
+        pass
+    return ""
+
 def bdmesg(just_clover = True):
-    b = None
-    if not just_clover:
-        b = _bdmesg(["ioreg","-l","-p","IOService","-w0"])
-    if not b:
+    b = "" if just_clover else _bdmesg(["ioreg","-l","-p","IOService","-w0"])
+    if b == "":
         b = _bdmesg(["ioreg","-l","-p","IODeviceTree","-w0"])
     return b
 
 def _bdmesg(comm):
-    # Runs the passed command and searches for "boot-log"
+    # Runs ioreg -l -p IODeviceTree -w0 and searches for "boot-log"
     p = subprocess.Popen(comm, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     bd, be = p.communicate()
     for line in bd.decode("utf-8").split("\n"):
@@ -23,6 +41,6 @@ def _bdmesg(comm):
             return binascii.unhexlify(line.split("<")[1].split(">")[0].encode("utf-8")).decode("utf-8")
         except:
             # Failed to convert
-            return None
+            return ""
     # Didn't find it
-    return None
+    return ""
